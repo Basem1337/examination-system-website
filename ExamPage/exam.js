3352755d0473e83b8cdc9dff1f6a88fb21899e79
AhmedSobhy <ahmedsobhi.fci@gmail.com>
2025-01-27 00:31:28 +0200

    Added a function for the time simulation with shake effect and color changing.

--- ExamPage/exam.js
+++ ExamPage/exam.js
@@ -9,3 +9,33 @@ $(".option").on("click", function () {
   }
 });
 /////////////////
+//////a setinterval function to simulate time and will be used to call the function timeout.
+//added a shake effect when turning red.
+let time = 6 * 60;
+
+let x = setInterval(() => {
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  $(".time").text(`${minutes}:${seconds}`);
+  if (time <= 90) {
+    $(".time").css("color", "#ffd500");
+  }
+  if (time === 30) {
+    $(".time").addClass("shake");
+  }
+  if (time <= 30) {
+    $(".time").css("color", "red");
+  }
+  if (time === 29) {
+    $(".time").removeClass("shake");
+  }
+  if (time <= 0) {
+    clearInterval(x);
+  } else {
+    time--;
+  }
+}, 1000);
+////////////////
